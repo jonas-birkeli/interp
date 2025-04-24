002b42c3cd186a7443dab75d36e5f2b861c1a67d
Jonas Birkeli <jonabi@stud.ntnu.no>
2025-04-24 14:40:23 +0200

    Create entry point, repl and file mode

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,27 @@
 module Main (main) where
 
 import Lib
+import System.Environment (getArgs)
+import System.Directory (doesFileExist)
 
+-- | The main entry point
 main :: IO ()
-main = someFunc
+main = do
+    args <- getArgs
+    mode <- determineRunMode args
+    executeMode mode
+
+
+-- | Determine the run mode based on command line arguments - File or not basically
+determineRunMode :: [String] -> IO RunMode
+determineRunMode = pure ReplMode
+determineRunMode (filePath:_) = do
+    fileExists <- doesFileExist
+    pure $ if fileExists
+        then FileMode filePath
+        else InvalidFileMode filepath
+
+-- | Execution modes for the interpeter
+data RunMode = ReplMode
+    | FileMode FilePath
+    | InvalidFileMode FilePath
