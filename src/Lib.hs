3e3d199c1d6f435c5f75949f810f236176bf44b4
Jonas Birkeli <jonabi@stud.ntnu.no>
2025-04-25 10:18:23 +0200

    Create skeleton for parsing

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,5 +2,31 @@ module Lib
     ( someFunc
     ) where
 
+-- | Placeholder function
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = putStrLn "Bprog Interpreter"
+
+-- | Evalutae a single line in the context for current state
+evalLine :: String -> State -> IO State
+
+-- | Show execution result with stack info
+showExecutionResult :: (State, Value) -> IO ()
+showExecutionResult (newstate, result) = do
+    putStrLn $ "Result: " ++ show result 
+    putSTrLn $ "Stack: " ++ formatStack (stack newState)
+
+-- | Formats stack for display
+formatStack :: Stack -> String
+formatStack = ("[" ++ ) . (++ "]") . intercalate ", " . map show -- dotfree stack
+-- (++ "] <- function , ("[" ++) <- function
+
+-- | Handle error in parsing
+handleParseError 
+
+-- | Parse and execute program
+parseAndExecute :: (String -> Either ParseError [Token])
+                -> String
+                -> State
+                -> ((State, Value) -> IO ())
+                -> IO State
+parseAndExecute parser program state resultHandler =
