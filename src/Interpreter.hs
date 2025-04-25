10b2b3343485805d37ad79837f28186ea8e47b16
Jonas Birkeli <jonabi@stud.ntnu.no>
2025-04-25 11:45:57 +0200

    Create token executor with template commands

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -13,3 +13,55 @@ initialState = State
         dictionary = Map.empty,
         stack = []
     }
+
+{-
+-- | Execute the program (list of tokens) with given state
+executeProgram :: [Token] -> State -> Either ProgramError (State, Value)
+executeProgram tokens state = do
+    finalState <- foldl (\stateResult token -> 
+            case stateResult of
+                Left err -> Left err
+                Right st -> executeToken token st)
+            (Right state)
+            tokens
+    case stack finalState of
+        [] -> Left ProgramFinishedWithNoValues
+        [v] -> Right (finalState, v)
+        vs -> Left (ProgramFinishedWithMultipleValues vs)
+        -}
+        
+        
+-- | Execute the program (list of tokens) with given state
+executeProgram :: [Token] -> State -> Either ProgramError (State, Value)
+executeProgram tokens state = do
+    finalSTate <- foldM executeToken state Tokens
+    extractFinalValue finalState
+
+-- | Extract the final value from state
+extractFinalValue :: State -> Either ProgramError (State, Value)
+extractFinalValue state = case stack state of
+    [] -> Left ProgramFinishedWithNoValues
+    [v] -> Right (state, v)
+    vs -> Left (ProgramFinishedWithMultipleValues vs)
+
+-- | Execute a single token with a given state
+executeToken :: Token -> State -> Either ProgramError State
+executeToken token = case token of
+    ValueToken value -> Right (pushValue value)
+    OperatorToken op -> executeOperator op
+    AssignmentToken -> executeAssignment
+    FunctionToken -> executeFunction
+    IfToken -> executeIf
+    TimesToken -> executeTimes
+    LoopToken -> executeLoop
+    MapToken -> executeMap
+    FoldlToken -> executeFoldl
+    EachToken -> executeEach
+    ExecToken -> executeExec
+
+
+
+
+-- | Push a value onto the stack
+pushValue :: Value -> State -> State
+pushValue value state = state { stack = value : stack state }
