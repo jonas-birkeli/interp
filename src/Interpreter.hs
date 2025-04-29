5e58766bf71817455843e920d385b83954c22ea6
Jonas Birkeli <jonabi@stud.ntnu.no>
2025-04-29 18:21:15 +0200

    Get function block from dict and execute

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -51,7 +51,7 @@ executeTokenStream (token:tokens) state =
 -- | Execute a single token with a given state
 executeToken :: Token -> State -> Either ProgramError State
 executeToken token s = case token of
-    ValueToken value -> Right (pushValue value s)
+    ValueToken value -> executeValue value s
     OperatorToken op -> executeOperator op s
     AssignmentToken -> executeAssignment s
     FunctionToken -> executeFunction s
@@ -63,6 +63,31 @@ executeToken token s = case token of
     IfToken -> Left $ UnknownSymbol "if token encountered out of context"
     TimesToken -> Left $ UnknownSymbol "times token encountered out of context"
     LoopToken -> Left $ UnknownSymbol "loop token encountered out of context"
+
+-- | Execute a value, handling symbol lookup
+executeValue :: Value -> State -> Either ProgramError State
+executeValue value s = case value of
+    SymbolValue name ->
+        -- Check for occurancei in dict
+        case Map.lookup name (dictionary s) of
+            Just quotation@(QuotationValue tokens) ->
+                executeQuotationOrValue quotation s
+            Just value' ->
+            -- Push the value if its not an quotation
+                Right $ pushValue value' s
+            Nothing ->
+                -- Push the symbol itself if not found
+                Right $ pushValue value s
+    _ -> Right $ pushValue value s
+
+-- | Execute a quotation or a direct value
+executeQuotationOrValue :: Value -> State -> Either ProgramError State
+executeQuotationOrValue value state = case value of
+    QuotationValue tokens -> 
+        -- Execute the quotation as a series of tokens
+        executeTokenStream tokens state >>= \(finalState, _) -> Right finalState
+    -- For non-quotation values, just push onto the stack
+    _ -> Right $ pushValue value state
 
 -- | Split token stream at the next quotation or single value
 splitAtQuotation :: [Token] -> Either ProgramError ([Token], [Token])
@@ -394,7 +419,7 @@ executeFunction state = do
     (symbolValue, state2) <- popValue state1
     case (quotation, symbolValue) of
         (QuotationValue _, SymbolValue name) ->
-            Right $ state2 { dictionary = Map.insert name quotation (dictionary state2) }
+            Right $ state2 { dictionary = Map.insert name quotation (dictionary state2 )}
         (_, _) -> Left $ ExpectedQuotation quotation
 
 -- | Execute exec operation
