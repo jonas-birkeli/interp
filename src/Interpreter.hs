7b4c12410a0273bb1c662441a12520a6f64c7fe3
Jonas Birkeli <jonabi@stud.ntnu.no>
2025-04-30 20:08:58 +0200

    Export initialstate

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -1,26 +1,20 @@
 module Interpreter
-    ( -- Core functions
-      executeProgram
-    , executeTokenStream
-    , initialState
-      -- Re-exports from submodules
-    , module Interpreter.Arithmetic
-    , module Interpreter.Comparison
-    , module Interpreter.Control
-    , module Interpreter.HigherOrder
-    , module Interpreter.Stack
-    , module Interpreter.List
-    , module Interpreter.String
-    , module Interpreter.IO
-    , module Interpreter.Variables
+    ( 
+        module Interpreter.Arithmetic,
+        module Interpreter.Comparison,
+        module Interpreter.TokenProcessor, 
+        module Interpreter.Stack,   
+        module Interpreter.List,
+        module Interpreter.String,
+        module Interpreter.IO,
+        module Interpreter.Variables,
+        module Interpreter.State
     ) where
 
-import Interpreter.Core
 import Interpreter.State 
 import Interpreter.Arithmetic
+import Interpreter.TokenProcessor
 import Interpreter.Comparison
-import Interpreter.Control
-import Interpreter.HigherOrder
 import Interpreter.Stack
 import Interpreter.List
 import Interpreter.String
