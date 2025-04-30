a2286677c281e86523d27656df29f61b1e0db0ad
Jonas Birkeli <jonabi@stud.ntnu.no>
2025-04-30 14:58:46 +0200

    Refactor string operations into own file

---
+++ src/Interpreter/String.hs
@@ -0,0 +1,44 @@
+module Interpreter.String
+    (
+        executeParseInteger,
+        executeParseFloat,
+        executeWords
+    ) where
+
+import Types (ProgramError(..), State(..), Value(..))
+import Interpreter.Stack (pushValue, popValue)
+import Text.Read (readMaybe)
+
+-- | Execute parseInteger operation
+executeParseInteger :: State -> Either ProgramError State
+executeParseInteger state = do
+    (value, state') <- popValue state
+    case value of
+        StringValue s ->
+            maybe
+                (Left $ NumberConversionError s)
+                (\i -> Right $ pushValue (IntValue i) state')
+                (readMaybe s)
+        _ -> Left $ ExpectedEnumerable value
+
+-- | Execute parseFloat operation
+executeParseFloat :: State -> Either ProgramError State
+executeParseFloat state = do
+    (value, state') <- popValue state
+    case value of
+        StringValue s ->
+            maybe
+                (Left $ NumberConversionError s)
+                (\f -> Right $ pushValue (FloatValue f) state')
+                (readMaybe s)
+        _ -> Left $ ExpectedEnumerable value
+
+-- | Execute words operation
+executeWords :: State -> Either ProgramError State
+executeWords state = do
+    (value, state') <- popValue state
+    case value of
+        StringValue s ->
+            let wordList = ListValue $ map StringValue (words s)
+            in Right $ pushValue wordList state'
+        _ -> Left (ExpectedEnumerable value)
