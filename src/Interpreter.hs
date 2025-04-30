54ba3fb4145e3a39f26f6a3d2f90100874d80b68
Jonas Birkeli <jonabi@stud.ntnu.no>
2025-04-30 15:15:13 +0200

    Export all refactored files from directory

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -1,16 +1,28 @@
-{-# LANGUAGE RankNTypes #-}
 module Interpreter
-    ( executeProgram
-    , initialState
+    ( -- Core functions
+      executeProgram
     , executeTokenStream
+    , initialState
+      -- Re-exports from submodules
+    , module Interpreter.Arithmetic
+    , module Interpreter.Comparison
+    , module Interpreter.Control
+    , module Interpreter.HigherOrder
+    , module Interpreter.Stack
+    , module Interpreter.List
+    , module Interpreter.String
+    , module Interpreter.IO
+    , module Interpreter.Variables
     ) where
 
-import Types (ProgramError(..), State(..), Token(..), Value(..))
-import qualified Data.Map as Map
-import Text.Read (readMaybe)
-import Control.Monad ((>=>), foldM)
-
-
-
-
-
+import Interpreter.Core (executeProgram, executeTokenStream)
+import Interpreter.State (initialState)
+import Interpreter.Arithmetic
+import Interpreter.Comparison
+import Interpreter.Control
+import Interpreter.HigherOrder
+import Interpreter.Stack
+import Interpreter.List
+import Interpreter.String
+import Interpreter.IO
+import Interpreter.Variables
